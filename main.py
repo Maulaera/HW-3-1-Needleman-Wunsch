215625c13b54879571e61e5094bc93cfdf02c463
maula <mrobi021@odu.edu>
2025-02-13 16:52:04 -0500

    optimal alignment calculations

--- main.py
+++ main.py
@@ -24,3 +24,22 @@ def needleman_wunsch(seq1, seq2, match_score,mismatch_score,gap_pen):
             dp[i][j] = max(match,delete,insert)
 
 # Find optimal alignment
+    aligned_seq1,aligned_seq2 = "",""
+    i,j = n,m
+
+    while i > 0 or j < 0:
+        current_score = dp[i][j]
+        if i > 0 and j > 0 and (current_score == dp[i-1][j-1] + 
+                            (match_score if seq1[i-1] == seq2[j-1] else mismatch_score)):
+            aligned_seq1 = seq1[i-1] + aligned_seq1
+            aligned_seq2 = seq2[j-1] + aligned_seq2
+            i -= 1
+            j -= 1
+        elif i > 0 and (current_score == dp[i-1][j] + gap_pen):
+            aligned_seq1 = seq1[i-1] + aligned_seq1
+            aligned_seq2 = "-" + aligned_seq2
+            i -= 1
+        else:
+            aligned_seq1 = "-" + aligned_seq1
+            aligned_seq2 = seq2[j-1] + aligned_seq2
+            j -= 1
